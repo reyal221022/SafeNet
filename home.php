143bbd5165b8a2b86e8dbbfcc73333a7f094c5b5
reyal221022 <esarkar221022@bscse.uiu.ac.bd>
2026-05-15 17:10:34 +0600

    Add basic features.

---
+++ home.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SafeNet</title>
+    <link rel="stylesheet" href="home1.css">
+</head>
+<body>
+    <div id="parent">  
+        <div id="bar">
+            <img src="logo.png" class="logo">
+            <h2 style="margin-top:1vh; margin-left:2vh ;font-size:6vh;  "> <a href="home.php" class="home-link">SafeNet</a></h2>
+              <button class="btn1" onclick="location.href='volunteer.html'">Volunteer</button>         
+             <button class="btn" onclick="location.href='post.php'">post</button>      
+              <button class="btn" onclick="location.href='foundation.html'">Foundation</button>        
+              <button class="btn" onclick="location.href='donation.html'">Donation</button>
+              <button class="btn" onclick="location.href='crisis_map.php'">Crisis Map</button>        
+              <button class="btn" onclick="location.href='create_event.html'">Create Event</button>
+              <button class="btn" onclick="location.href='unews.php'">Newsroom</button>
+              <button class="btn2" onclick="location.href='home.html'">log out</button>                      
+        </div>
+
+        <div id="slideshow">
+            <div id="container">
+                <div class="slide_image">
+                    <img src="new-image/slid1.jpg">
+                </div>
+                <div class="slide_image">
+                    <img src="new-image/slid2.jpg">
+                </div>
+                <div class="slide_image">
+                    <img src="new-image/slid3.jpg">
+                </div>
+            </div>
+        </div>
+
+        <div id="largbar">
+            <div class="txtcontainer">
+              <h1>Grasp the opportunity to <br>serve the country. </h1>
+              <br>
+              <h4> Embrance every opportunity, channel yourr skills, <br> and help the people in need.</h4>
+            </div>
+        
+                <img src="home4.jpg" class="img1">
+                <img src="brown1.jpg" class="img2">
+                <img src="relief.jpg" class="img3">
+           
+        </div>
+        <div class="div2">
+            <!---for white div of image and text-->
+            <div class="imgcontainer">
+           
+                <img src="brown2.jpg" class="img11">
+                <img src="3m.jpg" class="img21">
+                 <br>
+                 <img src="home2.jpg" class="img12">
+                 <img src="brown.jpg" class="img22">
+                
+            </div>
+            <div class="welcome">
+                <div class="txt">
+                    <h1> Welcome to our <br> Disaster Management System</h1>
+                    <h1>SafeNet</h1>
+                </div>
+            
+                <div class="details">
+                    <!---for vertical line and text-->
+                    <div class="vrt1">
+                        <div class="vl"></div>
+                        <h1 class="txt1">100 Active Volunteer</h1>
+                    </div>
+                    <div class="vrt1">
+                        <div class="vl1"></div>
+                        <h1 class="txt2">10 Popular Foundation</h1>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+              <!---for the header of features-->
+        <div class="feature">
+            <h1>Explore Our Services</h1>
+        </div>
+        <!--for the last div of feature button and text-->
+          <div class="div3">
+            <div class="innerdiv">
+                <img src="newsroom.jpg" class="news">
+                <button class="btnc" onclick="location.href='unews.php'">Newsroom</button>
+             <h1 class="title" >Stay updated with the latest news on disasters and relief efforts. Explore stories that keep you informed and inspired to help.</h1>
+            </div>
+            <div class="innerdiv">
+                <img src="create_event.png" class="crevent">
+                <button class="btnc" onclick="location.href='create_event.html'">Create Event</button>
+                <h1 class="title" >Easily create and manage events to support those in need. Join hands with others to make a meaningful impact.</h1>   
+            </div>
+            <div class="innerdiv">
+                <img src="cridis_map.jpg" class="crisis">
+                <button class="btnc" onclick="location.href='crisis_map.php'">Crisis-map</button>
+                <h1 class="title" >Your generosity can bring hope to those affected by disasters. Donate today and be a part of the change.</h1>
+            </div>
+            <div class="innerdiv">
+                <img src="donationbut.png" class="donation">
+                <button class="btnc" onclick="location.href='donation.html'">Donation</button>
+                <h1 class="title" >Discover areas most affected by calamities and where help is needed the most. Use the map to guide your efforts effectively.</h1><br>               
+            </div>
+          </div>
+
+          <!--for the footer-->
+          <div class="footer">
+            <div class="photo">
+                <img src="logo.png"  class="logo2">
+            <h1 style="font-size: 3.5vh; margin-left:7vh">SafeNet</h1>
+            </div>
+            <div class="first-div">
+                <p style="font-size:3.5vh; font-weight: bold">User :</p><br>
+                <p class="small_text">Create an Account.</p><br>
+                <p class="small_text"> Create an Event.</p><br>
+                <p class="small_text">Be a volunteer.</p><br>
+                <p class="small_text">Anyone can Donate.</p><br>
+
+            </div>
+            <div class="second_div">
+                <p style="font-size:3.5vh; font-weight: bold">Founmdation :</p><br>
+                <p class="small_text">Create an Account.</p><br>
+                <p class="small_text"> Analyze Crisis map.</p><br>  
+            </div>
+
+            <div class="third_div">
+                <p style="font-size:3.5vh; font-weight: bold">Contact us :</p><br>
+                <p class="small_text">Need any support call to 01347832465723.</p><br>
+                <p class="small_text">or email saifnet72@gmail.com.</p><br>
+            </div>
+
+            <div class="fourth_div">
+                <p style="font-size:3.5vh; font-weight: bold">Follow us on :</p><br>
+                <div class="conimg">
+                    <img src="fb.png" class="imgf">
+                    <img src="lnkdin.png" class="imgf">
+                    <img src="youtub.png" class="imgf">
+                    <img src="insta.jpg" class="imgf">
+                </div>
+            </div>
+
+          </div>
+            </div>
+</body>
+</html>
